1d60ace5072133283df2b66c2b7f8ce71ee73949
AlexanderMinkov <33556733+AlexanderMinkov@users.noreply.github.com>
2019-03-09 03:59:15 +0300

    Update zara.php

--- zara.php
+++ zara.php
@@ -1,9 +1,7 @@
 <?php
-
 // $document = phpQuery::newDocument($html);
 // $echo = $document->find('.content');
 ini_set('max_execution_time', 1000);
-
 include_once 'C:\Apache24\htdocs\www\parcers\libs\phpQuery-onefile.php';
 function curl_get_contents($url)
 {
@@ -21,9 +19,6 @@ function curl_get_contents($url)
 // $doc = phpQuery::newDocument($file);
 // $evr = $doc->find('.price _product-price');
 // echo $evr;
-
-
-
 	 $url;
 	 $file;
 	 $doc;
@@ -36,33 +31,33 @@ function curl_get_contents($url)
      $start = array();
      $end = array();
      $p_group = array();
-
+     $p_brand = array();
+     $p_category = array();
+     $gender = array();
 	
 		$url = "https://www.zara.com/ru/ru/muzhchiny-specialnye-ceny-l806.html?v1=1113505";
 		$file = curl_get_contents($url);
 		$doc = phpQuery::newDocument($file); 
 	
-
 	
         $count = 0;
 		foreach ($doc->find('._groups-wrap ul li') as $ev ) {
-
-
 		    $ev = pq($ev);
 		    if(($ev->find('a:eq(0)')->attr('href'))!=""){ 
-			    $p_link[] = $ev->find('a:eq(0)')->attr('href');
-			    $price_bf[] = $ev->find('.price._product-price .line-through')->attr('data-price');
-                $price_af[] = $ev->find('.price._product-price .sale')->attr('data-price');
+                $p_link[] = $ev->find('a:eq(0)')->attr('href');
+                $str = (string)$it->find('.price._product-price .line-through')->attr('data-price');
+                $str = preg_replace("/[^0-9]/", '', $str);
+                $price_bf[] = $str;
+                $str = (string)$it->find('.price._product-price .sale')->attr('data-price');
+                $str = preg_replace("/[^0-9]/", '', $str);
+                $price_af[] = $str;
                 $count++;
-
 				}
-
 		}
-
 		for($i=0;$i<$count;$i++){
 			if($i%2==0){
 				sleep(1);
-			}
+            }
 			$file1 = curl_get_contents($p_link[$i]);
 			$doc1 = phpQuery::newDocument($file1);
 			$img[] = $doc1->find('._seoImg.main-image:eq(0)')->attr('href');
@@ -71,16 +66,17 @@ function curl_get_contents($url)
             $start[] = null;
             $end[] = null;
             array_push($p_group, "Zara");
+            array_push($p_brand,'Zara');
+            array_push($p_category,'All');
+            array_push($gender,"M");
             
             
 	
         }	
         for($i=0;$i<sizeof($name);$i++){
             $name[$i] = preg_replace('^изображение 1 из^','',$name[$i]);
-
         }
         
-
             print_r($p_link);
 			echo '<br>';
 			print_r($price_bf);
@@ -99,20 +95,5 @@ function curl_get_contents($url)
             echo '<br>';
             print_r($p_group);
             echo '<br>';
-
             
-		
-
-	
-
-
-
-
-
-
-
-
-
-
-
-?> 
+            ?>
