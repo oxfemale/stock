5974458105b2645bb66d47b72dfcd3c1a952c7b9
AlexanderMinkov <33556733+AlexanderMinkov@users.noreply.github.com>
2019-03-10 22:19:21 +0300

    Add files via upload

--- wildberries/parce.php
+++ wildberries/parce.php
@@ -4,12 +4,17 @@ include_once 'C:\Apache24\htdocs\www\parcers\libs\phpQuery-onefile.php';
 
 
 
-function parce_it($iter,$url,$group,$category,&$p_link,&$price_bf,&$price_af,&$img,&$name,&$p_desc,&$start,&$end,&$p_group,&$p_brand,&$p_category){
+function parce_it($iter,$url,$group,$category,&$p_link,&$price_bf,&$price_af,&$img,&$name,&$p_desc,&$start,&$end,&$p_group,&$p_brand,&$p_category,&$gender){
 
   
     for($i=1;$i<$iter;$i++){
-        $url .= (string)$i;
-        $file = curl_get_contents($url);
+        $link = $url;
+        $link .= (string)$i;
+        
+        if($i%2==0){
+            sleep(1);
+        }
+        $file = curl_get_contents($link);
         $doc = phpQuery::newDocument($file);
     
         foreach($doc->find('.dtList') as $it){
@@ -17,16 +22,26 @@ function parce_it($iter,$url,$group,$category,&$p_link,&$price_bf,&$price_af,&$i
             $it = pq($it);
             array_push($p_link,(string)$it->find('.ref_goods_n_p')->attr('href'));
             if((string)$it->find('.thumbnail')->attr('data-original')==''){
-                array_push($img, (string)$it->find('.thumbnail')->attr('src'));
+                $str = (string)$it->find('.thumbnail')->attr('src');
+                $str = preg_replace('^c246x328^','big',$str);
+                array_push($img, $str);
             }
             else{
-                array_push($img, (string)$it->find('.thumbnail')->attr('data-original'));
+                $str = (string)$it->find('.thumbnail')->attr('data-original');
+                $str = preg_replace('^c246x328^','big',$str);
+                array_push($img,$str);
             }
-            array_push($price_af, (string)$it->find('.lower-price')->clone()->children()->remove()->end()->text());
-            array_push($price_bf, (string)$it->find('.price-old-block del')->clone()->children()->remove()->end()->text());
+            $pricea = (string)$it->find('.lower-price')->clone()->children()->remove()->end()->text();
+            $pricea = preg_replace('~\D+~','',$pricea);
+            array_push($price_af, $pricea);
+            $priceb = (string)$it->find('.price-old-block del')->clone()->children()->remove()->end()->text();
+            $priceb = preg_replace('~\D+~','',$priceb);
+            array_push($price_bf, $priceb);
             array_push($p_brand,(string)$it->find('.brand-name')->clone()->children()->remove()->end()->text());
             array_push($name,(string)$it->find('.goods-name')->clone()->children()->remove()->end()->text());
             array_push($p_group,$group);
+            array_push($gender,'M');
+
             $p_desc[] = null;
             $start[] = null;
             $end[] = null;
